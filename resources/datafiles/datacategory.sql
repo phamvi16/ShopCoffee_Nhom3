08759eca3b0779df41f14565cb65583d43da0dd9
tai vuong duy <taivuongduy2@gmail.com>
2020-10-09 12:41:52 +0700

    add data for cate and topping

---
+++ resources/datafiles/datacategory.sql
@@ -0,0 +1,8 @@
+insert into `category` (`Name`,`Image`) values
+('Cà phê',''),
+('Trà tươi',''),
+('Trà sữa',''),
+('Đá xay',''),
+('Bánh ngọt',''),
+('Bánh mặn','');
+
---
+++ resources/datafiles/datatopping.sql
@@ -0,0 +1,15 @@
+insert into `topping` (`Name`,`Price`,`Status`) values
+('Trân châu đen',5000,'Còn hàng'),
+('Trân châu trắng',5000,'Còn hàng'),
+('Trân châu hoàng kim',5000,'Còn hàng'),
+('Thạch rau câu vị cà phê',8000,'Còn hàng'),
+('Thạch rau câu vị dâu',8000,'Còn hàng'),
+('Thạch rau câu vị matcha',8000,'Còn hàng'),
+('Hạt thủy tinh nguyên vị',8000,'Còn hàng'),
+('Hạt thủy tinh vị dâu',8000,'Còn hàng'),
+('Hạt thủy tinh vị cam',8000,'Còn hàng'),
+('Thạch pudding trứng',10000,'Còn hàng'),
+('Thạch pudding phô mai',10000,'Còn hàng'),
+('Thạch pudding trái cây',10000,'Còn hàng'),
+('Bánh flan',15000,'Còn hàng');
+

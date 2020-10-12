82745fc9fb21cfffe9b0d79cbfa630c8215ccd77
Phương Lê Thị Trúc <55975124+phuongltt28@users.noreply.github.com>
2020-10-13 02:13:34 +0700

    Fix Add/Edit Product
    - Fixed Add/Edit product (80%)

--- resources/views/admin/editpro.blade.php
+++ resources/views/admin/editpro.blade.php
@@ -10,142 +10,135 @@
             <div class="col-md-12">
                 <div class="card ">
                     <div class="card-header ">
+                        @if(\Session::has('success'))
+                            <div class="alert-box success"><span>Success: </span> {{ \Session::get('success') }}</div>
+                            
+                         @elseif(\Session::has('error'))
+                            <div class="alert-box error"><span>Error: </span> {{ \Session::get('error') }}</div>
+                        @endif
                     <h4 class="card-title">Edit Product</h4>
                     </div>
                     <div class="card-body ">
-                            <form action="/admin/product/update" method="post" enctype="multipart/form-data">
+                            <form action="/admin/product/update" id="edit-pro-form" method="post" enctype="multipart/form-data">
                             @csrf
                             @if ($pro != NULL)
-                            <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>ID</label>
-                                                <input type="text" name="Id" class="form-control" value="{{$pro->Id}}">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Product Name</label>
-                                                <input type="text" class="form-control" name="Name" id="Name" value="{{$pro->Name}}" required>
-                                                @if($errors->has('Name'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Name') }}</div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Category</label>
-                                                <select name="Category" id="Category" class="form-control input-sm m-bot15">
-                                                    @if (!$categories->isEmpty())
-                                                        @foreach ($categories as $item)
-                                                            @if ($pro->category->first()->Id == $item->Id)
-                                                                <option selected value="{{ $item->Id }}">{{ $item->Name }}</option>
-                                                            @else
-                                                                <option value="{{ $item->Id }}">{{ $item->Name }}</option>
-                                                            @endif
-
-                                                        @endforeach
-                                                    @endif
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Description</label>
-                                                <textarea style="resize: none; border:1px solid #E3E3E3; border-radius: 30px;" rows="8" class="form-control" name="Description" id="Description" required>{{$pro->Description}}</textarea>
-                                                @if($errors->has('Description'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Description') }}</div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Size</label>
-                                                <label class="checkbox-container">Small
-                                                <input type="checkbox" name="Size[]" value="S">
-                                                <span class="checkmark"></span>
-                                                </label>
-
-                                                <label class="checkbox-container">Medium
-                                                <input type="checkbox" name="Size[]" value="M">
-                                                <span class="checkmark"></span>
-                                                </label>
-
-                                                <label class="checkbox-container">Large
-                                                <input type="checkbox" name="Size[]" value="L">
-                                                <span class="checkmark"></span>
-                                                </label>
-
-                                                <label class="checkbox-container">None
-                                                <input type="checkbox" name="Size[]" value="None">
-                                                <span class="checkmark"></span>
-                                                </label>
-                                                @if($errors->has('Size'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Size') }}</div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                     <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Price</label>
-                                                <input type="number" class="form-control" name="Price" id="Price" value="{{$pro->Price}}" required min="0">
-                                                @if($errors->has('Price'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Price') }}</div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Sale Price</label>
-                                                <input type="number" class="form-control" name="Sale_Price" id="Sale_Price" value="{{$pro->Sale_Price}}" required min="0">
-                                                @if($errors->has('Sale_Price'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Sale_Price') }}</div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                     <div class="row">
-                                        <div class="col-md-6 pr-1">
-                                            <div class="form-group">
-                                                <label>Image</label>
-                                                <input style="opacity: 1; position: static" type="file" class="form-control" name="Image" id="Image" value="{{$pro->Image}}">
-                                                <img src="{{asset('ProductImages/Products/').'/'.$pro->Image}}">
-                                                @if($errors->has('Image'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Image') }}</div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div class="row">
                                     <div class="col-md-6 pr-1">
                                         <div class="form-group">
-                                            <label>Visibility</label>
-                                            <input type="text" class="form-control" name="Visibility" id="Visibility" value="{{$pro->Visibility}}" required>
-                                            @if($errors->has('Email'))
-                                                    <div class="alert-box error"><span>error: </span> {{ $errors->first('Email') }}</div>
+                                            <label>ID</label>
+                                            <input type="text" name="Id" class="form-control" value="{{$pro->Id ?? ""}}" readonly>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                            <label>Product Name</label>
+                                            <input type="text" class="form-control" name="Name" id="Name" value="{{$pro->Name ?? ""}}" required>
+                                            @if($errors->has('Name'))
+                                                <div class="alert-box error"><span>error: </span> {{ $errors->first('Name') }}</div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                        <label>Category</label>
+                                        @if (!$categories->isEmpty())
+                                            @foreach ($categories as $item)
+                                                <label class="checkbox-container">{{ $item->Name ?? ""}}
+                                                @if (in_array($item->Id, $commonCategories))
+                                                    <input type="checkbox" class="Category" name="Category[]" value="{{ $item->Id ?? ""}}" checked>
+                                                    <span class="checkmark"></span>
+                                                @else
+                                                    <input type="checkbox" class="Category" name="Category[]" value="{{ $item->Id ?? "" }}">
+                                                    <span class="checkmark"></span>
                                                 @endif
+                                                </label>
+                                                
+                                            @endforeach
+                                        @endif
+                                        @if($errors->has('Category'))
+                                            <div class="alert-box error"><span>error: </span> {{ $errors->first('Category') }}</div>
+                                        @endif
+                                    </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                            <label>Description</label>
+                                            <textarea style="resize: none; border:1px solid #E3E3E3; border-radius: 30px;" rows="8" class="form-control" name="Description" id="Description" required>{{$pro->Description ?? ""}}</textarea>
+                                            @if($errors->has('Description'))
+                                                <div class="alert-box error"><span>error: </span> {{ $errors->first('Description') }}</div>
+                                            @endif
                                         </div>
                                     </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                            <label>Size:</label>
+                                            <input type="text" class="form-control" name="Size" readonly value="{{$pro->Size ?? ""}}">
+                                        </div>
+                                    </div>
+                                </div>
+                                 <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                            <label>Price</label>
+                                            <input type="number" class="form-control" name="Price" id="Price" value="{{$pro->Price ?? ""}}" required min="0">
+                                            @if($errors->has('Price'))
+                                                <div class="alert-box error"><span>error: </span> {{ $errors->first('Price') }}</div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                            <label>Sale Price</label>
+                                            <input type="number" class="form-control" name="Sale_Price" id="Sale_Price" value="{{$pro->Sale_Price ?? ""}}" required min="0">
+                                            @if($errors->has('Sale_Price'))
+                                                <div class="alert-box error"><span>error: </span> {{ $errors->first('Sale_Price') }}</div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                 <div class="row">
+                                    <div class="col-md-6 pr-1">
+                                        <div class="form-group">
+                                            <label>Image</label>
+                                            <input style="opacity: 1; position: static" type="file" class="form-control" name="Image" id="Image" value="{{$pro->Image ?? ""}}">
+                                            <img src="{{asset('ProductImages/Products/').'/'.$pro->Image}}" width="400px" height="400px" alt="{{$pro->Name ?? ""}}">
+                                            @if($errors->has('Image'))
+                                                <div class="alert-box error"><span>error: </span> {{ $errors->first('Image') }}</div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 pr-1">
+                                    <div class="form-group">
+                                        <label>Visibility</label>
+                                        <input type="text" class="form-control" name="Visibility" id="Visibility" value="{{$pro->Visibility ?? ""}}" required>
+                                        @if($errors->has('Email'))
+                                            <div class="alert-box error"><span>error: </span> {{ $errors->first('Email') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
 
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                            <button type="submit" name="" class="btn btn-info">Save</button>
-                                            </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <button type="submit" name="btnSave" class="btn btn-info">Save</button>
+                                            <a href="/admin/product" name="btnBack" class="btn back">Back</a>
                                         </div>
                                     </div>
+                                </div>
                             @else
+                            <div>An unexpected error occurred. Failed to show product detail. 
+                                <p>Please checks if this product exists.</p></div>
                             @endif
 
                             </form>

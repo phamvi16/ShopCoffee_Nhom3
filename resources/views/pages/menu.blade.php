ccda87ee7b8db3a8214ee1c961f3e7006deead9f
thuynt <thuybinh11997@gmail.com>
2020-10-28 23:13:16 +0700

    update from modal in menu  UI

--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -47,36 +47,77 @@
                     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h5 class="modal-title text-black" id="exampleModalLabel">Get A Quote</h5>
+                            <img  src="{{asset('Page/img/1.jpg')}}" style="height: 100px;width:100px"/>
+                          <h4 class="modal-title text-black m-5" id="exampleModalLabel">Trà đào <p>size:M</p></h4>
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                           </button>
                         </div>
                         <div class="modal-body p-5">
                           <form action="#" method="post">
-                                <div class="form-group row">
-                                  <div class="col-md-6 mb-4 mb-lg-0">
-                                    <input type="text" class="form-control border" placeholder="First name">
+                                <div class="form-group row modal-header">
+
+                                  <div class="mb-4 mb-lg-0 mr-5"> Size:</div>
+                                  <div class="form-check form-check-inline mr-5">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                    <label class="form-check-label" for="inlineRadio1">S</label>
                                   </div>
-                                  <div class="col-md-6">
-                                    <input type="text" class="form-control border" placeholder="First name">
+                                  <div class="form-check form-check-inline mr-5">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                    <label class="form-check-label" for="inlineRadio2">M</label>
+                                  </div>
+                                  <div class="form-check form-check-inline mr-4">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" >
+                                    <label class="form-check-label" for="inlineRadio3">L</label>
+                                  </div>
+
+                                </div>
+
+
+
+                                <div class="form-group row modal-header">
+                                  <div class="col-md-12">
+                                    <div class="m-4">Topping:</div>
+                                    <div class="text-center">
+                                    <div class="form-check form-check-inline mr-4">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                        <label class="form-check-label" for="inlineRadio1">Trân châu trắng</label>
+                                      </div>
+                                      <div class="form-check form-check-inline mr-5">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                        <label class="form-check-label" for="inlineRadio1">Trân châu đen</label>
+                                      </div>
+                                      <div class="form-check form-check-inline mr-5">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                        <label class="form-check-label" for="inlineRadio1">Thạch trái cây</label>
+                                      </div>
+                                    </div>
+
                                   </div>
                                 </div>
 
                                 <div class="form-group row">
                                   <div class="col-md-12">
-                                    <input type="text" class="form-control border" placeholder="Email address">
+                                    <textarea name="" id="" class="form-control border" placeholder="Ghi chú." cols="30" rows="2"></textarea>
                                   </div>
-                                </div>
 
-                                <div class="form-group row">
-                                  <div class="col-md-12">
-                                    <textarea name="" id="" class="form-control border" placeholder="Write your message." cols="30" rows="10"></textarea>
+                                </div>
+                                <span class="col-sm-2 qty mt-4" >
+                                    <input class="quant-input-modal pl-3" id="qty2632" type="number" min="1" value="1" placeholder="1" >
+
+                                </span>
+                                
+                                   <div class="form-group row ">
+                                  <div class="col-md-6 ml-auto">
+                                    <input type="" class="btn btn-block btn-primary text-white py-3 px-5 add-to-cart" value="Giá:" >
+                                   
                                   </div>
                                 </div>
                                 <div class="form-group row">
                                   <div class="col-md-6 ml-auto">
-                                    <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5" value="Send Message">
+                                    <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5 add-to-cart" value="Thêm vào giỏ" >
+                                   
+                                    
                                   </div>
                                 </div>
                               </form>

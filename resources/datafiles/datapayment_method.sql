6bcf7f533649ddc82a5e27ab4f6008a69aa5778f
Tài <taivuongduy2@gmail.com>
2020-10-29 19:57:34 +0700

    Datafile

---
+++ resources/datafiles/datapayment_method.sql
@@ -0,0 +1,6 @@
+insert into `payment_method` (`Name`,`Status`) values
+('Thanh Toán Trực Tiếp','Hỗ trợ'),
+('Thanh Toán Qua Thẻ Tín Dụng','Hỗ trợ'),
+('Thanh Toán Qua Ví Điện Tử Paypal','Hỗ trợ'),
+('Thanh Toán Qua Ví Điện Tử MoMo','Hỗ trợ'),
+('Thanh Toán Qua Ví Điện Tử Zalopay','Không hỗ trợ');

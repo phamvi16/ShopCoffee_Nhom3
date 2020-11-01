7088ddb9e841053b34ff063f76df9d460bf2cb12
Khoa <ohwhynotme1999@gmail.com>
2020-11-01 20:44:14 +0700

    read session

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Topping;
+use DB;
 class CheckoutController extends Controller
 {
     public function index(){
-        return view('pages.checkout');
+        $all_topping = DB::table('topping')->get();
+        return view('pages.checkout',compact('all_topping'));
     }
 }
--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -1,6 +1,40 @@
 @extends('main_layout')
 @section('content')
+<?php 
+  Session::forget('cart');
+  $item = [
+    'id'=>'1',
+    'name'=>'Espresso / Americano',
+    'image'=>'product-20201012134003.jpeg',
+    'id_product_size'=>'2',
+    'size'=>'M',
+    'price_buy'=>'40000', // = sale_price
+    'sugar'=>'30',
+    'ice'=>'50',
+    'hot'=>'70',
+    'topping'=>[
+        "2"=>"5000"
+    ]
+  ];
+  Session::push('cart', $item);
+  $item = [
+    'id'=>'5',
+    'name'=>'Cà Phê Sữa',
+    'image'=>'product-20201012134003.jpeg',
+    'id_product_size'=>'13',
+    'size'=>'S',
+    'price_buy'=>'25000', // = sale_price
+    'sugar'=>'30',
+    'ice'=>'50',
+    'hot'=>'70',
+    'topping'=>[
+        "2"=>"5000",
+        "3"=>"1000"
+    ]
+  ];
+  Session::push('cart', $item);
 
+ ?>
 
 <div class="container checkout mb-4">
     <div class="col-xs-12 col-sm-7 wrapper-checkout-left">
@@ -51,7 +85,8 @@
             <span class="required req-phone"></span>
         </label>
 
-        <textarea class="textarea pt-2 mt-3 form-group" name="note" id="note" cols="83" rows="5" placeholder="Note.."></textarea>
+        <textarea class="textarea pt-2 mt-3 form-group" name="note" id="note" cols="83" rows="5"
+            placeholder="Note.."></textarea>
 
         <button class="btn btn-primary chkout-sub mt-4">CHECK OUT</button>
 
@@ -60,57 +95,70 @@
     <div class="col-sm-5 wrapper-checkout-right pr-0">
         <div class="d-flex align-items-center">
             <div class="order-sum mt-4">Order Summary</div>
-            <div class="edit mt-4">Edit</div>
+            <div class="edit mt-4"><a href="/gio-hang">Edit</a></div>
         </div>
+
+        <?php
+            $data = Session::get('cart');
+
+            $totalPrice_Topping=0;
+
+            $totalPrice_Product=0;
+
+            for($i=0; $i < Count($data);$i++){
+                
+                $totalPrice_Product += $data[$i]['price_buy'];
+        echo '<div class="hr mt-4"></div>
+                <div class="d-flex align-items-center">
+                    <img class="img-order-sum" src="/ProductImages/Products/'.$data[$i]['image'].'" alt="">
+                    <div class="flex-column" style="flex-grow: 1">
+                        <div class="ml-4 mb-2 name-sum">'.$data[$i]['name'].'</div>
+                        <div class="ml-4">Topping:';
+                       
+            $list_topping = collect($data[$i]['topping'])->keys();
+            for($y=0;$y<Count($list_topping);$y++){
+                $item = collect($all_topping)->where('Id',$list_topping[$y])->first();
+                echo  '<div><small><i>'.$item->Name.'</i></small></div>';
+                $totalPrice_Topping+=$item->Price;
+            }
+            echo'</div>
+                    </div>
+                    <div class="mr-4">'.number_format($data[$i]['price_buy']).' VNĐ</div>
+                    
+                </div>
+                ';
+            }
+
+        ?>
+
         <div class="hr mt-4"></div>
-        <div class="d-flex align-items-center">
-            <img class="img-order-sum" src="https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg" alt="">
-            <div class="flex-column" style="flex-grow: 1">
-                <div class="ml-4 mb-2 name-sum">Tionem ullam corporis sample G</div>
-                <div class="ml-4">30.000 VNĐ</div>
-            </div>
 
-            <div class="mr-4">30.000 VNĐ</div>
-        </div>
-        <div class="hr mt-4"></div>
-
-        <div class="d-flex align-items-center">
-            <img class="img-order-sum" src="https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg" alt="">
-            <div class="flex-column" style="flex-grow: 1">
-                <div class="ml-4 mb-2 name-sum">Tionem ullam corporis sample G</div>
-                <div class="ml-4">30.000 VNĐ</div>
-            </div>
-
-            <div class="mr-4">30.000 VNĐ</div>
-        </div>
-        <div class="hr mt-4"></div>
-
-        <h3 class="cre-acc">APPLY COUPON</h3>
+        <h3 class="cre-acc">Mã Giảm Giá</h3>
         <div class="hr-small"></div>
         <div class="hr"></div>
         <div>
-            <input class="form-control in-coupon" style="display: inline;" type="text" placeholder="Your coupon code..">
-            <button class="btn btn-primary btn-apply ml-2">APPLY</button>
+            <input class="form-control in-coupon" style="display: inline;" type="text" placeholder="nhập mã giảm giá..">
+            <button class="btn btn-primary btn-apply ml-2">Apply</button>
         </div>
 
         <div class="hr mt-5"></div>
 
 
         <div class="d-flex">
-            <div class="subtotal mb-2">Subtotal</div>
-            <div class="mr-4">30.000 VNĐ</div>
+            <div class="subtotal mb-2">Tiền Topping</div>
+            <div class="mr-4">{{number_format($totalPrice_Topping)}} VNĐ</div>
         </div>
 
         <div class="d-flex">
             <div class="subtotal mb-2">Shipping</div>
-            <div class="mr-4">30.000 VNĐ</div>
+            <div class="mr-4">Free Ship</div>
         </div>
 
         <div class="hr mt-4 mb-4"></div>
 
         <div class="d-flex wrapper-total">
-            <div class="total-w">TOTAL</div>
-            <div class="mr-4 total">90.000 VNĐ</div>
+            <div class="total-w">Tổng Cộng</div>
+            <div class="mr-4 total">{{number_format($totalPrice_Topping + $totalPrice_Product)}} VNĐ</div>
         </div>
 
 

218b50c4a161fa9c921e104c0701d8db0d47ad6f
nhintt <nhintt2411@gmail.com>
2020-11-06 23:00:42 +0700

    Fix UI sort by status on pro-manage

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -7,24 +7,28 @@
             <div class="col-md-12">
                 <div class="card ">
                     <div class="card-header ">
-                    <h4 class="card-title">Danh sách đơn hàng</h4>
+                        <h4 class="card-title">Danh sách đơn hàng</h4>
                     </div>
 
-                   <div  class="d-flex"  >
-                        <span class="ml-3 mt-1">Lọc theo ngày tháng:</span>
-                        <input type="date" id='birthday' name='birthday' class=" ml-3 mr-3" placeholder="" value="">
-                        <input type="date" id='birthday' name='birthday' class="ml-3" placeholder="" value="">
-                        <div class="btn-group ml-5 " >
-                            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              Lọc theo Status
+                    <div class="d-flex align-items-center">
+                        <div style="flex-grow: 1">
+                            <span class="ml-3 mt-1">Lọc theo ngày tháng:</span>
+                            <input type="date" id='birthday' name='birthday' class=" ml-3 mr-3" placeholder="" value="">
+                            <input type="date" id='birthday' name='birthday' class="ml-3" placeholder="" value="">
+                        </div>
+
+                        <div class="btn-group ml-5 ">
+                            <button type="button" class="btn btn-danger dropdown-toggle mr-5" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                                Lọc theo trạng thái
                             </button>
                             <div class="dropdown-menu">
-                              <a class="dropdown-item" href="#">Status1</a>
-                              <a class="dropdown-item" href="#">Status1</a>
-                              <a class="dropdown-item" href="#">Status1</a>
+                                <a class="dropdown-item" href="#">Đang xử lý</a>
+                                <a class="dropdown-item" href="#">Đang giao</a>
+                                <a class="dropdown-item" href="#">Đã giao</a>
                             </div>
-                          </div>
-                   </div>
+                        </div>
+                    </div>
                     <div class="card-body ">
                         <div class="table-responsive">
                             <table class="table">
@@ -60,7 +64,7 @@
                                         </td>
                                     </tr>
                                 </tbody>
-                             </table>
+                            </table>
                         </div>
                     </div>
                 </div>
